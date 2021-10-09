58a522869218533c9acb9c6966df576aec5ad08b
guricerin <chanbo1e9@gmail.com>
2021-10-09 21:14:34 +0900

    rm unuse

--- db/initdb.d/setup.sql
+++ db/initdb.d/setup.sql
@@ -1,13 +1,3 @@
--- drop database if exists "sns_db";
--- create database "sns_db";
-
--- drop user if exists "sns_user"@"%";
--- create user "sns_user"@"%" identified by 'sns123456789';
--- grant all on "sns_db".* to "sns_user"@"%";
--- flush privileges;
-
--- use "sns_db";
-
 drop table if exists "users";
 drop table if exists "sessions";
 drop table if exists "cigarettes";
